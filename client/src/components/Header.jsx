128f0e8094a10c83ba7f39dc0449eabffcb8b6a4
Rahat Husain khan <123046685+Rahat-coder971@users.noreply.github.com>
2025-04-12 19:11:19 +0530

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,23 +2,23 @@ import { useEffect, useState } from "react";
 
 function Header({activetab,setactivetab}){
 
-    const [debounce,setdebounce]=useState('');
+    // const [debounce,setdebounce]=useState('')
     
     
-// concept of debounce implemneted,
-    useEffect(()=>{
-        const timer=setTimeout(() => {
-            if(debounce === 'home' || debounce ==='Shirts' || debounce ==='TShirts' || debounce ==='Jeans'){
-           setactivetab(debounce)
-            console.log(activetab)
-            }
-            else{
-                console.log('no result found');
+// concept of debounce implemneted, just for practice
+    // useEffect(()=>{
+    //     const timer=setTimeout(() => {
+    //         if(debounce === 'home' || debounce ==='Shirts' || debounce ==='TShirts' || debounce ==='Jeans'){
+    //        setactivetab(debounce)
+    //         console.log(activetab)
+    //         }
+    //         else{
+    //             console.log('no result found');
                 
-            }
-        }, 1000);
-        return ()=>{clearTimeout(timer);}
-    },[debounce])
+    //         }
+    //     }, 1000);
+    //     return ()=>{clearTimeout(timer);}
+    // },[debounce])
 
     return(
         <div className=" flex ">
@@ -28,23 +28,23 @@ function Header({activetab,setactivetab}){
 
            <img className="w-[35%] sm:w-[10%]" src="https://cdn.shopify.com/static/images/flags/au.svg?width=40" alt="" />
 
-            {/* <select onChange={(e)=>{
+            <select onChange={(e)=>{
                 {setactivetab(e.target.value)}
             }} className="w-[65%] sm:w-[50%]" name="" id="product">
                 <option value="Shirts">Shirts</option>
                 <option value="TShirts">TShirts</option>
                 <option value="Jeans">Jeans</option>
-            </select> */}
-            <input list="tab"  className="bg-red-400 w-[70%]" onChange={(e)=>{
+            </select>
+{/*             <input list="tab"  className="bg-red-400 w-[70%]" onChange={(e)=>{
                 
                 setdebounce(e.target.value)
-            }} type='search' />
-         <datalist id="tab">
+            }} type='search' /> */}
+{/*          <datalist id="tab">
         <option value="home">home</option>
         <option value="Shirts">shirts</option>
         <option value="Jeans">jeans</option>
         <option value="Tshirts">Tshirts`</option>
-         </datalist>
+         </datalist> */}
         </div>
 
         </div>
